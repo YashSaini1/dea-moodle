ce9100a49eb1ec327b1bbdc11566322c6876e7cc
YashSaini1 <ys3394508@gmail.com>
2026-05-29 16:15:56 +0530

    fix

--- local/myapi/db/upgrade.php
+++ local/myapi/db/upgrade.php
@@ -77,7 +77,7 @@ function xmldb_local_myapi_upgrade($oldversion)
         upgrade_plugin_savepoint(true, 2026050203, 'local', 'myapi');
     }
 
-    if ($oldversion < 2026050208) {
+    if ($oldversion < 2026050210) {
         $table = new xmldb_table('local_myapi_module_group_map');
 
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
@@ -97,7 +97,62 @@ function xmldb_local_myapi_upgrade($oldversion)
             $dbman->create_table($table);
         }
 
-        upgrade_plugin_savepoint(true, 2026050208, 'local', 'myapi');
+        upgrade_plugin_savepoint(true, 2026050210, 'local', 'myapi');
+    }
+
+    if ($oldversion < 2026050211) {
+        $table = new xmldb_table('local_myapi_user');
+
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('assignto', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('tier', XMLDB_TYPE_CHAR, '20', null, null, null, null);
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_index('userid_idx', XMLDB_INDEX_NOTUNIQUE, ['userid']);
+
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        $table = new xmldb_table('local_myapi_course');
+
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('isinternal', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, 0);
+        $table->add_field('estimatedtime', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, '');
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('courseid', XMLDB_KEY_FOREIGN, ['courseid'], 'course', ['id']);
+
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        $table = new xmldb_table('local_myapi_module_group_map');
+
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('coursemoduleid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('groupid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_index('idx_mgm_coursemodule', XMLDB_INDEX_NOTUNIQUE, ['coursemoduleid']);
+        $table->add_index('idx_mgm_group', XMLDB_INDEX_NOTUNIQUE, ['groupid']);
+        $table->add_index('idx_mgm_course', XMLDB_INDEX_NOTUNIQUE, ['courseid']);
+        $table->add_index('idx_unique_module_group', XMLDB_INDEX_UNIQUE, ['coursemoduleid', 'groupid']);
+
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        upgrade_plugin_savepoint(true, 2026050211, 'local', 'myapi');
     }
 
     return true;
